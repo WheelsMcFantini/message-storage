b075fd45c30a1a6ad34a864f670ed7edbe78556f
Wheels <95301184+WheelsMcFantini@users.noreply.github.com>
2023-09-26 17:25:47 -0700

    cleanup, move to flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, jsonify, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
     LoginManager,
     UserMixin,
@@ -8,68 +9,59 @@ from flask_login import (
     logout_user,
 )
 #from flask_wtf.csrf import CSRFProtect
-import pdb
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
-from sqlalchemy.orm import sessionmaker, declarative_base
+from forms.forms import RegistrationForm, LoginForm
 from flask_login import UserMixin
 
-
-engine = create_engine("sqlite:///message1.db", echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+db = SQLAlchemy()
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
 app.config.update(
     DEBUG=True,
     SECRET_KEY="secret_sauce",
     SESSION_COOKIE_HTTPONLY=True,
     REMEMBER_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE="Strict",
+    SESSION_COOKIE_SAMESITE="Strict"
 )
 
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True)
+    password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"<User(id='{self.id}', username='{self.username}', password='{self.password}')>"
+    
+class Message(db.Model):
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    message = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<User(id='{self.id}', user_id='{self.user_id}', message='{self.message}')>"
+
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+    
 login_manager = LoginManager()
 login_manager.init_app(app)
 #login_manager.session_protection = "strong"
 login_manager.login_view = "home"
 #csrf = CSRFProtect(app)
 
-#DB
-import sqlite3
-db_locale = "message.db"
-print(f"opening connection to db")
-#con = sqlite3.connect(db_locale)
-#temporary users
-
-Base = declarative_base()
-class User(Base, UserMixin):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    password = Column(String)
-
-    def __repr__(self):
-        return f"<User(id='{self.id}', username='{self.username}', password='{self.password}')>"
-    
-
-class Message(Base):
-    __tablename__ = "messages"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    message = Column(String)
-
-    def __repr__(self):
-        return f"<User(id='{self.id}', user_id='{self.user_id}', message='{self.message}')>"
-
-#return the user object if the user exists
 def get_user(user_id: int):
     print(f"the user id is {user_id}")
     if (user_id == "None"):
         return None
     else:
-        user_by_id = (session.query(User).filter_by(id=int(user_id)).first())
+        user_by_id = (db.session.query(User).filter_by(id=int(user_id)).first())
         print(user_by_id)
         if not user_by_id:
             return None
@@ -95,10 +87,11 @@ def user_loader(id: int):
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def home(path):
+    print(f"the db id is:{id(db)}")
+    print(f"the db tables are:{db.metadata.tables.keys()}")
     if current_user.is_authenticated:
-        #check for a message, if there is one, show it
         user = get_user(current_user.id)
-        message_row = (session.query(Message).filter_by(user_id=user["id"]).first())
+        message_row = (db.session.query(Message).filter_by(user_id=user["id"]).first())
         if message_row is not None:
             print("message for user")
             return render_template("main_for_user.html", message=message_row.message)
@@ -115,7 +108,7 @@ def login():
     print(form.data)
     error = None
     if request.method == 'POST' and form.validate():
-            user = session.query(User).filter(User.username == form.data["username"]).first()
+            user = db.session.query(User).filter(User.username == form.data["username"]).first()
             if user.password == form.data["password"]:
                 user_model = User()
                 user_model.id = user.id
@@ -148,7 +141,7 @@ def check_session():
     return jsonify({"login": False})
 
 
-@app.route("/api/logout")
+@app.route("/api/logout", methods=["POST"])
 @login_required
 def logout():
     logout_user()
@@ -158,56 +151,40 @@ def logout():
 @login_required
 def message(): 
     user = get_user(current_user.id)
-    message_row = (session.query(Message).filter_by(user_id=user["id"]).first())
+    message_row = (db.session.query(Message).filter_by(user_id=user["id"]).first())
     message_entry = Message(user_id=user["id"], message=request.form['message'])
     if request.method == 'POST':
-        message_row = (session.query(Message).filter_by(user_id=user["id"]).first())
+        message_row = (db.session.query(Message).filter_by(user_id=user["id"]).first())
         if message_row is None:
             message_entry = Message(user_id=user["id"], message=request.form['message'])
-            session.add(message_entry)
-            session.commit()
+            db.session.add(message_entry)
+            db.session.commit()
+            return render_template("main_for_user.html", message=message_row.message)
         else:
             message_row.message = request.form['message']
-            session.commit()
+            db.session.commit()
+            return render_template("main_for_user.html", message=message_row.message)
     if request.method == 'GET':
         #check for a message and get it if there is one
-        #message_row = (session.query(Message).filter_by(user_id=User.id).first())
+        message_row = (db.session.query(Message).filter_by(user_id=user["id"]).first())
         #if message_row is None or message_row.message is None:
         if message_row is None:
             print(f"No message saved for user: {User.username}")
-            return ""
-        return message_row.message
+            return render_template("main_for_user.html")
+        return render_template("main_for_user.html", message=message_row.message)
 
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25), validators.DataRequired("please input a valid username")])
-    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email("That's not a valid address")])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
-
-class LoginForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25), validators.DataRequired("please input a valid username")])
-    password = PasswordField('Password', [
-        validators.DataRequired()
-    ])##### DO THIS NEXT MAKE YOUR LOGIN FORM NICE
-    #Then figure out why your session doesn't seem to be doing anything
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    print(f"the db id is:{id(db)}")
+    print(f"the db tables are:{db.metadata.tables.keys()}")
     form = RegistrationForm(request.form)
-    #breakpoint()
     print(form.validate())
     if request.method == 'POST' and form.validate():
-        query = f"INSERT INTO users (username, password) VALUES ('{form.username.data}', '{form.password.data}')"
-        tires_user = User(username=form.username.data, password=form.password.data)
-        session.add(tires_user)
-        session.commit()
-        print(query)
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
         flash('Thanks for registering')
         return redirect(url_for('home'))
     return render_template('register.html', form=form)
--- forms/forms.py
+++ forms/forms.py
@@ -1,11 +1,17 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    username = StringField('Username', [validators.Length(min=4, max=25), validators.DataRequired("please input a valid username")])
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email("That's not a valid address")])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25), validators.DataRequired("please input a valid username")])
+    password = PasswordField('Password', [
+        validators.DataRequired()
+    ])
